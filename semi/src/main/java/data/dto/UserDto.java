61b0df71608f2a15f4987643be98e111b9c9ff08
wlsgud623 <wlsgud623@hanmail.net>
2022-05-17 15:56:56 +0900

    Update UserDto.java

--- semi/src/main/java/data/dto/UserDto.java
+++ semi/src/main/java/data/dto/UserDto.java
@@ -9,20 +9,6 @@ import lombok.Data;
 @Data
 @Alias("user")
 public class UserDto {
-<<<<<<< HEAD
-	private String UserID;
-	private String name;
-	private String password;
-	private String tier;
-	private String hp;
-	private String email;
-	private String photo;
-	private String intro;
-	private String ownrecipe;
-	private String currentrecipe;
-	private String scraprecipe;
-	private Timestamp joinday;
-=======
 	private String UserID; //사용자 ID
 	private String name; // 사용자 이름
 	private String password; //사용자 비밀번호
@@ -35,5 +21,4 @@ public class UserDto {
 	private String currentrecipe; // 사용자가 최근 열람한 레시피
 	private String scraprecipe; // 사용자가 스크랩한 레시피
 	private Timestamp joinday; // 회원 가입 일자
->>>>>>> branch 'main' of https://github.com/wlsgud623/bitcamp_901_05.git
 }
